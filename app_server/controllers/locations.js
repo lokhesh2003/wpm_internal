71452a5c58fd30d342cc3ec4a932d7b4af3335a4
lokhesh2003 <lokheshreddi@gmail.com>
2025-08-21 11:17:43 +0530

    main

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,17 @@
+/* GET 'home' page */
+const homelist = (req, res) => {
+res.render('index', { title: 'Lokhesh 23EG107E16 wpm_internal' });
+};
+/* GET 'Location info' page */
+const locationInfo = (req, res) => {
+res.render('index', { title: 'Location info' });
+};
+/* GET 'Add review' page */
+const addReview = (req, res) => {
+res.render('index', { title: 'Add review' });
+};
+module.exports = {
+homelist,
+locationInfo,
+addReview
+};
